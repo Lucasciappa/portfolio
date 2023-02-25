e2897ed8de7f679a9107b89f1287c36fd0d595b8
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-02-25 15:08:22 -0300

    add: blog page

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -7,23 +7,23 @@ const BlogPage = () => {
 	return (
 		<>
 			<SEO
-				title="Blog - Deus Gitonga"
-				keywords="front-end development, JavaScript, web development, design, best practices, industry trends"
-				description="Read about the latest technologies, best practices, and trends in the industry, and learn how to improve your skills as a Front-End and JavaScript Developer."
+				title="Blog - Lucas Ciapparelli"
+				keywords="Lucas Ciapparelli, Front-End Developer, Full Stack Developer, JavaScript Developer, React Developer, Next.js Developer"
+				description="Descubre mas sobre Lucas Ciapparelli. Full Stack developer con experiencia en la creación de aplicaciones SEO friendly y dinamicas"
 			/>
 
 			<div className="space-y-6 py-6">
 				<h1 className="mb-1 text-3xl font-bold tracking-tight text-zinc-800 dark:text-white md:text-4xl">
-					Lorem ipsum...
+					Mi Blog <span className="text-2xl text-zinc-600 dark:text-gray-400">(Sección en mantenimiento)</span>
 				</h1>
 				<p className="text-[15px] sm:text-[14px] md:text-[15px] leading-7 text-gray-500 dark:text-gray-400">
-					I share my experiences and insights on the latest technologies, best practices, and trends in the industry. Whether you're just starting out or are a seasoned developer, I hope you'll find something here that inspires you and helps you grow in your craft.
+					Aquí compartiré mis experiencias y conocimientos sobre las últimas novedades de tecnología, las mejores prácticas y tendencias. Mi idea es poder ayudar e inspirar a quien lo necesite. Espero que pronto esté disponible
 				</p>
 				<div className="relative">
 					<input
 						type="text"
-						placeholder="Search articles"
-						aria-label="Search articles"
+						placeholder="Buscar artículos"
+						aria-label="Buscar artículos"
 						className="w-full text-[15px] rounded border border-gray-500 bg-portfolio-bg py-2 px-3 pl-12 transition-colors duration-200 ease-linear focus:border-accent-5 focus:outline-none"
 						id="search"
 					/>
