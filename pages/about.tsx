ff81a4b34619dafb57591bde29f567a5de099d77
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-02-25 13:22:41 -0300

    add: about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,17 +6,17 @@ const About = () => {
 	return (
 		<>
 			<SEO
-				title="About - Deus Gitonga"
-				keywords="Deus Gitonga, Front-End Developer, JavaScript Developer, React Developer, Next.js Developer"
-				description="Learn more about Deus Gitonga, a Front-End and JavaScript Developer with experience in creating dynamic, user-friendly websites and applications."
+				title="Sobre mi - Lucas Ciapparelli"
+				keywords="Lucas Ciapparelli, Front-End Developer, Full Stack Developer, JavaScript Developer, React Developer, Next.js Developer"
+				description="Descubre mas sobre Lucas Ciapparelli. Full Stack developer con experiencia en la creación de aplicaciones SEO friendly y dinamicas"
 			/>
 
 			<div className="space-y-2 py-6 md:space-y-5">
 				<h1 className="mb-1 text-3xl font-bold tracking-tight text-zinc-800 dark:text-white md:text-4xl">
-					About Me
+					Sobre mi -
 				</h1>
 				<p className="text-[15px] leading-7 text-gray-500 dark:text-gray-400">
-					Hello there{" "}
+					Hola a todos{" "}
 					<span role="img" aria-label="waving hand" className="wave text-2xl">
 						👋
 					</span>
@@ -24,7 +24,7 @@ const About = () => {
 				</p>
 			</div>
 			<div className="mb-8 leading-7 space-y-4 ">
-				{["I am a Front-End & JavaScript Developer based in Kenya with a passion for creating dynamic and user-friendly digital experiences.", "Recent projects have involved working with a variety of clients and honing my skills in front-end technologies such as React JS and Next.js. I'm always eager to learn and experiment with new tools and techniques.", "In addition to my professional experiences, I hold a Degree in Mathematics & Computer Science from Jomo Kenyatta University of Agriculture and Technology. My education has provided a foundation for my technical skills and has taught me the importance of designing user-centered experiences."].map((paragraph, i) => (
+				{["Mi nombre es Lucas, tengo 25 años y vivo en Capital Federal, Argentina. Mi pasión es la innovación y apostar sobre nuevas tecnologías, son el camino para un mundo con mas oportunidades. Promuevo habilidades blandas para el desarrollo e innovación en equipo.", "Mi pasión por el desarollo de software empezó hace 4 años cuando me inscribí al curso de Codo a Codo - Full Stack con Python. Con el paso del tiempo fuí formándome mas como persona, aprendiendo a desenvolverme en equipo y generando autonomía.", " Disfruto llevar a cabo la maquetación e interactividad de interfaces web, así como la optimización para el buen posicionamiento y el rendimiento de carga de sitios y aplicaciones web.", "Con respecto a mi experiencia laboral, cuento con 2 años trabajando en una Startup española. Utilizamos el stack MEARN. Next.js para la web y Angular para el panel de admin. Para el Backend Node.js y MongoDB."].map((paragraph, i) => (
 					<p className="text-[15px] leading-7 text-gray-700 dark:text-gray-400" key={i}>
 						{paragraph}
 					</p>
