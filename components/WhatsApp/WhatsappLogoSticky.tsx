4f2aa8f145554126f78c87530f32d94fc89a07c7
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-02-26 23:44:17 -0300

    remove: CustomImage

--- components/WhatsApp/WhatsappLogoSticky.tsx
+++ components/WhatsApp/WhatsappLogoSticky.tsx
@@ -1,23 +1,22 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
-import { CustomImage } from "../UI/CustomImage";
 
 const WhatsappLogoSticky = () => {
     const router = useRouter();
     return (
-        <div className="fixed bottom-20 right-4 md:right-8 md:bottom-28">
+        <div className="fixed bottom-20 right-2 md:right-6 md:bottom-24">
             <a
                 title="Mandanos un mensaje"
                 href={`https://api.whatsapp.com/send/?phone=541131063791&text=Hola+Lucas%21%0D%0A%0D%0AQuiero+mas+informaci%C3%B3n+sobre+ti&app_absent=0`}
                 target="_blank"
                 rel="noreferrer"
-                className="block w-12 h-12 md:w-14 md:h-14 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12 relative"
+                className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12 relative"
             >
-                <CustomImage
-                    imgsrc={"/whatsapp-logo.webp"}
+                <Image
+                    src={"/whatsapp-logo.webp"}
                     alt={"logo WhatsApp"}
-                    layout={"fill"}
-                    objectFit={"cover"}
-                    quality={2}
+                    width={500}
+                    height={500}
                 />
             </a>
         </div>
